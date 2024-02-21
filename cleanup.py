d834a0409dd6a4da5c3e9859f3c1d239719dceea
gravitcapa <sedelnikov@hotmail.com>
2024-02-20 21:27:53 -0500

    Add files via upload

--- cleanup.py
+++ cleanup.py
@@ -6,22 +6,37 @@ from logging.handlers import TimedRotatingFileHandler
 from logging import handlers
 import cameraconfig
 
-def delete_oldest_files(folder, space_to_free):
+def delete_oldest_files(folder):
     logging.info(f"Disk space cleanup started. folder:{folder}")
-    files = [(f, os.path.getmtime(os.path.join(folder, f)), os.path.getsize(os.path.join(folder, f))) for f in os.listdir(folder)]
-    files.sort(key=lambda x: x[1])
 
-    for file in files:
-        
-        file_path = os.path.join(folder, file[0])
-        os.remove(file_path)
-        space_to_free -= file[2]
-        if space_to_free <= 0:
-            break
+    disk_usage = psutil.disk_usage(cameraconfig.archive_folder)
+    total_space = disk_usage.total
+    free_space = disk_usage.free
+    used_space = total_space - free_space
+    target_free_space = total_space * (cameraconfig.target_free_space_percentage / 100)
+    space_to_free = target_free_space - free_space
 
-        logging.info(f"Deleted: {file_path} created dt: {time.ctime(file[1])} size: {file[2]}")
-        
-    logging.info(f"Disk space cleanup completed. folder:{folder}")
+    logging.info(f"Total disk space: {total_space / (1024 ** 3):.2f} GB")
+    logging.info(f"Available disk space: {free_space / (1024 ** 3):.2f} GB")
+    logging.info(f"Used disk space: {used_space / (1024 ** 3):.2f} GB")
+    logging.info(f"Target free disk percent: {cameraconfig.target_free_space_percentage} space: {target_free_space / (1024 ** 3):.2f} GB")
+    logging.info(f"Space_to_free: {space_to_free / (1024 ** 3):.2f} GB")
+
+    if space_to_free > 0 :
+        files = [(f, os.path.getmtime(os.path.join(folder, f)), os.path.getsize(os.path.join(folder, f))) for f in os.listdir(folder)]
+        files.sort(key=lambda x: x[1])
+
+        for file in files:
+            
+            file_path = os.path.join(folder, file[0])
+            os.remove(file_path)
+            space_to_free -= file[2]
+            if space_to_free <= 0:
+                break
+
+            logging.info(f"Deleted: {file_path} created dt: {time.ctime(file[1])} size: {file[2]}")
+    else:
+        logging.info(f"Disk space cleanup completed. folder:{folder}")
 
 if __name__ == "__main__":
 
@@ -33,27 +48,10 @@ if __name__ == "__main__":
     try:
         
         logging.info(f"Application started")
-     
-        disk_usage = psutil.disk_usage(cameraconfig.archive_folder)
-        total_space = disk_usage.total
-        free_space = disk_usage.free
-        used_space = total_space - free_space
-        target_free_space = total_space * (cameraconfig.target_free_space_percentage / 100)
-        space_to_free = target_free_space - free_space
-
-        logging.info(f"Total disk space: {total_space / (1024 ** 3):.2f} GB")
-        logging.info(f"Available disk space: {free_space / (1024 ** 3):.2f} GB")
-        logging.info(f"Used disk space: {used_space / (1024 ** 3):.2f} GB")
-        logging.info(f"Target free disk percent: {cameraconfig.target_free_space_percentage} space: {target_free_space / (1024 ** 3):.2f} GB")
-        logging.info(f"Space_to_free: {space_to_free / (1024 ** 3):.2f} GB")
         
-        if space_to_free > 0 :
-            delete_oldest_files(cameraconfig.archive_folder, space_to_free)
-            delete_oldest_files(cameraconfig.videos_folder, space_to_free)
-            delete_oldest_files(cameraconfig.log_folder, space_to_free)
-            logging.info("Disk space cleanup completed.")
-        else:
-            logging.info("No need to clean up disk space.")
+        delete_oldest_files(cameraconfig.archive_folder)
+        delete_oldest_files(cameraconfig.videos_folder)
+        #delete_oldest_files(cameraconfig.log_folder)
 
         logging.info(f"Application finished")
         
